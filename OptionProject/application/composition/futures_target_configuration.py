829e89f93ef6049313d8026129c1c67593b1b782
Leedalsoo <ldsphm@naver.com>
2026-09-11 00:24:24 +0900

    Delete OptionProject directory
    결정

--- OptionProject/application/composition/futures_target_configuration.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-class FuturesTargetConfigurationError(ValueError):
-    pass
-@dataclass(frozen=True)
-class FuturesTargetConfiguration:
-    pass
