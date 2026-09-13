37c38c8b421e5e6eb54b021882b5292b8ed8fb65
Leedalsoo <ldsphm@naver.com>
2026-09-13 12:35:41 +0900

    fix: restore authoritative VSSF settlement engine

---
+++ environments/virtual/authoritative_vssf/settlement_engine.py
@@ -0,0 +1,5 @@
+class SettlementEngine:
+    def __init__(self, account): self.account=account
+    def perform_eod_settlement(self, final_settlement_price):
+        self.account.update_tick_price(final_settlement_price)
+        return self.account.ledger_engine.record_settlement("EOD", self.account.realized_pnl, self.account.unrealized_pnl, self.account.balance)
