ef3dec152ab50b44e36e79f90fd2f7b87b1649cf
Leedalsoo <ldsphm@naver.com>
2026-09-13 12:32:22 +0900

    fix: restore authoritative VSSF ledger engine

---
+++ environments/virtual/authoritative_vssf/ledger_engine.py
@@ -0,0 +1,20 @@
+from decimal import Decimal
+
+class LedgerEngine:
+    def __init__(self):
+        self.transactions = []
+
+    @staticmethod
+    def _decimal(value):
+        return value if isinstance(value, Decimal) else Decimal(str(value))
+
+    def record_settlement(self, settlement_type, realized_pnl, unrealized_pnl, balance_after, **kwargs):
+        row = {
+            "type": "SETTLEMENT",
+            "settlement_type": settlement_type,
+            "realized_pnl": self._decimal(realized_pnl),
+            "unrealized_pnl": self._decimal(unrealized_pnl),
+            "balance_after": self._decimal(balance_after),
+        }
+        self.transactions.append(row)
+        return row
